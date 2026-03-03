35f8a4e87318a8c47c5694fc46b97e2b00751305
phucbtt <bt.thanhphuc@gmail.com>
2026-03-03 22:12:26 +0700

    .

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
-export const SITE_TITLE = "Phuc | IT Helpdesk";
+export const SITE_TITLE = "PHUCBTT";
 export const SITE_DESCRIPTION =
   "Personal blog of Phuc – IT Helpdesk focused on system stability, automation, and continuous improvement.";
